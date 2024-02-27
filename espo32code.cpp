a21ae481edb1630c35d09613521e6abdac0f7f9c
DavidKeough <158479870+DavidKeough@users.noreply.github.com>
2024-02-27 09:24:26 -0500

    Update espo32code.cpp

--- espo32code.cpp
+++ espo32code.cpp
@@ -17,9 +17,17 @@
 #define KI 0.1
 #define KD 0.1
 
+// PID variables
+int PID_P = 0;
+int PID_I = 0;
+int PID_D = 0;
+int PID_Error = 0;
+int PID_Value = 0;
+
 float currentTemperature = 0.0;
 float previousError = 0.0;
-float integralTerm = 0.0;
+// float integralTerm = 0.0;
+float elapsedTime, Time, timePrev;
 
 
 MAX6675 thermocouple(MAX6675_SCK_PIN, MAX6675_CS_PIN, MAX6675_SO_PIN);
@@ -27,6 +35,7 @@ MAX6675 thermocouple(MAX6675_SCK_PIN, MAX6675_CS_PIN, MAX6675_SO_PIN);
 void setup() {
 	Serial.begin(9600);
 	pinMode(MOSFET_PIN, OUTPUT);
+	Time = millis();
 	Serial.println("MAX6675K Thermocouple Test");
 }
 
@@ -36,6 +45,8 @@ void loop() {
 	float temperature = thermocouple.readCelsius();
 
 
+
+	// Might help catch errors
 	if (temperature == NAN) {
 		Serial.println("Unable to read temperature :(");
 
@@ -44,9 +55,14 @@ void loop() {
 	}
 
 	
-	//currentTemperature = temperature;  // Update current temperature
+	currentTemperature = temperature;  // Update current temperature
 
-	////Perform PID control
+
+	/*
+	* ORIGNAL CODE
+	*/
+
+	//Perform PID control
 	//float error = SETPOINT_TEMPERATURE - temperature;
 	//integralTerm += error; // Accumulate error for integral term
 	//float derivativeTerm = error - previousError; // Calculate derivative term
@@ -55,23 +71,46 @@ void loop() {
 
 	//float output = (KP * error) + (KI * integralTerm) + (KD * derivativeTerm); // Calculate PID output
 
-	//Serial.print("PID output");
-	//Serial.println(output);
 
-	//// Clip the output to ensure it's within valid range
-	//output = constrain(output, 0.0, 255.0);
+	/*
+	* STOLEN CODE - Incorporates time into the derivative equation (rate of change)
+	*/
 
-	// Apply PWM to MOSFET pin to control heating element
+	PID_Error = SETPOINT_TEMPERATURE - temperature;
 	
-	if (temperature >= SETPOINT_TEMPERATURE) {
-		analogWrite(MOSFET_PIN, 255);
+	//Calculate the P value
+	PID_P = KP * PID_Error;
+
+	//Calculate the I value in a range on +-3 //? Not sure why this is necessary but im assuming it is. 
+	if (-3 < PID_Error < 3) 
+	{
+		PID_I = PID_I + (KI * PID_Error);
 	}
+
+	//Derivative equation using real time.
+	timePrev = Time;                            // the previous time is stored before the actual time read
+	Time = millis();                            // actual time read
+	elapsedTime = (Time - timePrev) / 1000;
+
+	//Now we can calculate the D calue
+	PID_D = KD * ((PID_Error - previousError) / elapsedTime);
+	//Final total PID value is the sum of P + I + D
+	PID_Value = PID_P + PID_I + PID_D;
+
+	
+	float PMW_Out = constrain(PID_Value, 0.0, 255.0); //clip output PMW signal
+
+	analogWrite(MOSFET_PIN, PMW_Out); //write PMW to mosfet 
+	previousError = PID_Error;
+	Serial.print("PID output");
+	Serial.println(PMW_Out);
+
 
 	// Print temperature and PID components to serial monitor
 	Serial.print("Temperature: ");
 	Serial.print(temperature);
-	Serial.println(" °C");
+	Serial.println(" Â°C");
 	Serial.print("PID Output: ");
-	Serial.println(output);
+	Serial.println(PMW_Out);
 	delay(1000); // Delay for readability
 }
